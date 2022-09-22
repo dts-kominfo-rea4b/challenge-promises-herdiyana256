f814450c0ca1ffd0ee2023d534babf339143fe6b
herdiyan_adam_putra <herdiyanadam256@gmail.com>
2022-09-23 04:49:27 +0700

    Submit Challenge : Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput= async(state) => {
+    var x=0;
+    await promiseTheaterIXX().then((value)=> {
+        value.forEach(emosi => {
+            if(emosi.result==state) {
+                x+=1;
+            }
+        });
+    });
+await promiseTheaterVGC().then((value) => {
+    value.forEach(emosi=> {
+        if(emosi.hasil==state) {
+            x+=1;
+             }
+        });
+    });
+
+    return  x;
+};
+
+
+
 
 module.exports = {
   promiseOutput,
